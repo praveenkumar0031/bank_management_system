dd1ae989827922883c7e634d906d47ebc83429e0
Praveen Kumar S <152008068+praveenkumar0031@users.noreply.github.com>
2025-03-04 20:54:50 +0530

    Create App.java
    basic template(ui)

---
+++ App.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class App{
+  public static void main(String[] args){
+Scanner in=new Scanner(System.in);
+System.out.println("Welcome to bank management system");
+System.out.print("___________________________________");
+while(true){
+    System.out.print("Enter your choise:\n 1.create Account \n 2.view balance \n 3.withdraw amount \n 4.deposit amount \n 5.exit");
+    int ch=in.nextInt();
+    switch(n){
+      case 1:
+          break;
+      case 2:
+          break;
+      case 3:
+          break;
+      case 4:
+          break;
+      case 5:
+          exit(0);
+          break;
+      default:
+          System.out.print("Invalid choise");
+      }
+    }
+  }
+}
