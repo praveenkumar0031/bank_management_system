a1b9635d999903fcb2e9d618b11761d3803534ad
Praveen kumar S <praveen0031kumar@gmail.com>
2025-08-05 16:30:02 +0530

    User ready

--- Account.java
+++ Account.java
@@ -1,57 +1,123 @@
 
 import BankBackend.*;
-class Account{
-    Backend db=new Backend();
-    String acholder="";
+
+class Account {
+    Backend db = new Backend();
+    String acholder = "";
     long acno;
-    String actype="";
-    String acstatus="INACTIVE";
-    float acbal=0;
-    Account(String acholder,String actype,float acbal,int freeno){
-        if(isValidAccount(acholder,actype)){
-        this.acholder=acholder.toUpperCase();
-        this.actype=actype.toUpperCase();
-        this.acbal=acbal;
-        this.acno=generateAcNO(freeno);
-        this.acstatus="ACTIVE";
-        db.insertValues( this.acno, this.acholder, this.acbal, this.acstatus, this.actype);
-        System.out.println("--------------------|Account created |-------------------- \n");
+    String actype = "";
+    String acstatus = "INACTIVE";
+    float acbal = 0;
+
+    public void createAccount(String acholder, String actype, float acbal, int freeno) {
+        if (isValidAccount(acholder, actype)) {
+            this.acholder = acholder.toUpperCase();
+            this.actype = actype.toUpperCase();
+            this.acbal = acbal;
+            this.acno = generateAcNO(freeno);
+            this.acstatus = "ACTIVE";
+            db.insertValues(this.acno, this.acholder, this.acbal, this.acstatus, this.actype);
+            System.out.println("--------------------|Account created |-------------------- \n");
         }
     }
-    //validate name-- full_alphabet and acc type(savings,current,fd,rd) I/P->(name,acctype) O/P->T/F
-    private static boolean isValidAccount(String name,String actype){
-        for(char c:name.toCharArray()){
-            if(!Character.isAlphabetic(c) && c!=' '){
+
+    // validate name-- full_alphabet and acc type(savings,current,fd,rd)
+    // I/P->(name,acctype) O/P->T/F
+    private static boolean isValidAccount(String name, String actype) {
+        for (char c : name.toCharArray()) {
+            if (!Character.isAlphabetic(c) && c != ' ') {
                 return false;
             }
         }
-        String type=actype.toUpperCase();
-        if(type.equals("SAVINGS")||type.equals("CURRENT")||type.equals("FD")||type.equals("RD")){
-        return true;
-        }
-        else{
-            String msg="Savings,Current,Fd,Rd";
+        String type = actype.toUpperCase();
+        if (type.equals("SAVINGS") || type.equals("CURRENT") || type.equals("FD") || type.equals("RD")) {
+            return true;
+        } else {
+            String msg = "Savings,Current,Fd,Rd";
             invalidMsg(msg);
             return false;
         }
     }
-    //method should have 4 digit no only
-    private static long generateAcNO(int lastno){
-        return (832000070000l+lastno);
+
+    // method should have 4 digit no only
+    private static long generateAcNO(int lastno) {
+        return (832000070000l + lastno);
     }
-    public void removeAccount(long ac_no){
-            
-            if(db.containsAccount(ac_no)){
-                db.removeFromDb(ac_no);
-        }
-        else{
+
+    public void removeAccount(long ac_no) {
+
+        if (db.containsAccount(ac_no)) {
+            db.removeFromDb(ac_no);
+        } else {
             System.out.print("Invlaid Account Number");
         }
     }
-    private static void invalidMsg(String msg){
-        System.out.println("Invalid option ! choose ("+msg+")");
+
+    private static void invalidMsg(String msg) {
+        System.out.println("Invalid option ! choose (" + msg + ")");
     }
-    public void demo(){
-        System.out.print("Run...");
+
+    // balance
+    public void Viewbalance(long ac_no) {
+        if (db.containsAccount(ac_no)) {
+            System.out.println("-----Account Balance-----");
+            db.ViewblanceDB(ac_no);
+        } else {
+            System.out.println("Account not available!");
+        }
+    }
+
+    public void withdrawMoney(long ac_no, float amt) {
+        if (db.containsAccount(ac_no)) {
+            float cr_amt = db.fetchAmount(ac_no);
+            if (cr_amt > amt) {
+                db.changeAccountAmount(ac_no, amt, cr_amt, 'W');
+            } else {
+                System.out.println("Insufficient balance Rs." + cr_amt);
+            }
+        } else {
+            System.out.println("Account not available!");
+        }
+    }
+
+    public void depositMoney(long ac_no, float amt) {
+        if (db.containsAccount(ac_no)) {
+            float cr_amt = db.fetchAmount(ac_no);
+
+            db.changeAccountAmount(ac_no, amt, cr_amt, 'D');
+
+        } else {
+            System.out.println("Account not available!");
+        }
+    }
+
+    public void DeactivateAccount(long ac_no) {
+        if (db.containsAccount(ac_no)) {
+            db.DeactivateAccountDb(ac_no);
+        } else {
+            System.out.println("Account not available!");
+        }
+    }
+
+    public void TransferMoney(long sac_no, long rac_no, float amt) {
+        if (db.containsAccount(sac_no)) {
+            System.out.println("Sender Account Verified...");
+            float cr_amt = db.fetchAmount(sac_no);
+            if (cr_amt > amt) {
+                db.changeAccountAmount(sac_no, amt, cr_amt, 'W');
+            } else {
+                System.out.println("Insufficient balance in Sender's Acount Rs." + cr_amt);
+            }
+
+            if (db.containsAccount(rac_no)) {
+                System.out.println("Reciver Account Verified...");
+                float r_cr_amt = db.fetchAmount(rac_no);
+
+                db.changeAccountAmount(rac_no, amt, r_cr_amt, 'D');
+
+                
+            }
+        }
+
     }
 }
--- App.java
+++ App.java
@@ -1,101 +1,109 @@
 import java.util.*;
 //import BankBackend.*;
 
-public class App{
-    static Scanner in = new Scanner(System.in);
-public static int  choiseMsg(){
-    
-    System.out.println("\nEnter your choise: ");
-    System.out.println(" 1.create Account \n" + 
-                " 2.View Balance \n" + 
-                " 3.Withdraw Amount \n" + 
-                " 4.Deposit Amount \n" + 
-                " 5.Money Transfer \n"+
-                " 6.Quit");
-    System.out.print(" ");
-    int ch=in.nextInt();
-    return ch;
-}
-public static void main(String[] args){
+public class App {
+  static Scanner in = new Scanner(System.in);
 
-    //ac.demo();
-    //ac.removeAccount(832000070025L);
-String an;
-Float amt;
-System.out.println("Bank management system");
-System.out.println("___________________________________");
-while(true){
-    int ch=choiseMsg();
-    switch(ch){
-      case 1:
-        System.out.print("  Enter account holder name: ");
-        in.nextLine();
-        String name=in.nextLine();
-        //System.out.print("  Enter account number: ");
-        //Long ac=in.nextLong();
-        
-        System.out.println();
-        System.out.print("  Enter account Type(SAVINGS/CURRENT/FD/RD): ");
-        String type=in.next();
-        System.out.print("  Enter Initial Deposit amount(rs): ");
-        amt=in.nextFloat();
-        int no=(int)(Math.random() * 9000) + 1000;
-        Account ac=new Account(name,type,amt,no);
-        System.out.print("Account Number: "+ac.acno);
-        
-        //System.out.print();
-        break;
-      /*case 2:
-          System.out.print("Enter account number: ");
-       an=in.next();
-       if(!sb.containsKey(an)){
-        System.out.println("Account not available!");
-        }
-        else{
-       Account b=sb.get(an);
-       balance(b);
-        }
-        break;
-      case 3:
-          System.out.print("Enter account number: ");
-         an=in.next();
-         System.out.print("Enter withdrawalamount: ");
-         amt=in.nextInt();
-         
-         if(!sb.containsKey(an)){
-            System.out.println("Account not available!");
-          }
-         else{
-         Account wa=sb.get(an);
-          withdraw(wa,amt);
-         }
+  public static int choiseMsg() {
+
+    System.out.println("\nEnter your choise: ");
+    System.out.println(" 1.create Account \n" +
+        " 2.View Balance \n" +
+        " 3.Withdraw Amount \n" +
+        " 4.Deposit Amount \n" +
+        " 5.Money Transfer \n" +
+        " 6.De-activate\n" +
+        " 7.Quit");
+    System.out.print(" ");
+    int ch = in.nextInt();
+    return ch;
+  }
+
+  public static long getAccountNo() {
+    System.out.print("Enter Account Number (83200007xxxx) :");
+    long acno = in.nextLong();
+    return acno;
+  }
+
+  public static void fundTransfer(Account ac) {
+    System.out.print("Enter Sender Account Number (83200007xxxx) : ");
+    long s_acno = in.nextLong();
+    System.out.print("Enter Receiver Account Number (83200007xxxx) : ");
+    long r_acno = in.nextLong();
+    System.out.print("\nEnter Transfer Amount: Rs.");
+    float amt = in.nextFloat();
+    ac.TransferMoney(s_acno, r_acno, amt);
+
+  }
+
+  public static void main(String[] args) {
+
+    // ac.demo();
+    // ac.removeAccount(832000070025L);
+    long acno;
+    Float amt;
+    Account ac = new Account();
+    System.out.println("Bank management system");
+    System.out.println("___________________________________");
+    while (true) {
+      int ch = choiseMsg();
+      switch (ch) {
+        case 1:
+          System.out.print("  Enter account holder name: ");
+          in.nextLine();
+          String name = in.nextLine();
+          // System.out.print(" Enter account number: ");
+          // Long ac=in.nextLong();
+
+          System.out.println();
+          System.out.print("  Enter account Type(SAVINGS/CURRENT/FD/RD): ");
+          String type = in.next();
+          System.out.print("  Enter Initial Deposit Amount :Rs.");
+          amt = in.nextFloat();
+          int no = (int) (Math.random() * 9000) + 1000;
+          ac.createAccount(name, type, amt, no);
+          System.out.print("Account Number: " + ac.acno);
+
+          // System.out.print();
           break;
-      case 4:
-          System.out.print("Enter account number: ");
-         an=in.next();
-         System.out.print("Enter depositamount: ");
-         amt=in.nextInt();
-         if(!sb.containsKey(an)){
-            System.out.println("Account not available!");
-          }
-         else{
-            Account da=sb.get(an);
-            deposit(da,amt);
-            
-         }
+        case 2:
+          acno = getAccountNo();
+
+          ac.Viewbalance(acno);
+
           break;
-        */
-      case 6:
+        case 3:
+          acno = getAccountNo();
+
+          System.out.print("Enter withdrawal Amount: Rs.");
+          amt = in.nextFloat();
+          ac.withdrawMoney(acno, amt);
+
+          break;
+        case 4:
+          acno = getAccountNo();
+          System.out.print("Enter deposit Amount: Rs.");
+          amt = in.nextFloat();
+          ac.depositMoney(acno, amt);
+          break;
+        case 5:
+          fundTransfer(ac);
+          break;
+        case 6:
+          System.out.println("Deactivation of Account");
+          acno = getAccountNo();
+          ac.DeactivateAccount(acno);
+          break;
+        case 7:
           System.out.println("exiting ....");
           System.out.println("Thanks for using");
           System.exit(0);
           break;
-      default:
+        default:
           System.out.print("Invalid choise");
 
       }
     }
-    
-    
+
   }
 }
